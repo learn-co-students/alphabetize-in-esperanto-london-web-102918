7b63d5d65c33722249b48708bbfaafed0a30f4b8
Paul Clark <clark.paul123@gmail.com>
2018-10-25 22:00:25 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,33 @@
+# ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+# use sort_by |phrase| phrase first letter alphabetically
+
+
+# does not work because of dodgy characters
+# def alphabetize(arr)
+#  arr.sort_by do |phrase| phrase.downcase
+#  end
+# end
+
 def alphabetize(arr)
-  # code here
+# sets the alphabet with new characters
+
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+# sorts english characters
+
+  arr.sort_by do |phrase| phrase
+
+# maps the esperanto alphabet
+
+    phrase.chars.map do |letter|
+
+# compares each letter of the phrase to it's index in the alphabet
+# still in sort_by method 
+
+    esperanto_alphabet.index(letter)
+    
+    end
+  end
 end
+
+# https://medium.com/@ensallee/the-difference-between-sort-and-sort-by-in-ruby-253171f52b15
